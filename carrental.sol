db5449160a48ee4d8477aac41e5b36c93de670f5
Shuyj07 <96546922+Shuyj07@users.noreply.github.com>
2023-02-04 14:17:44 +0800

    Create carrental.sol

---
+++ carrental.sol
@@ -0,0 +1,204 @@
+pragma solidity 0.5.1;
+import "hardhat/console.sol";
+pragma experimental ABIEncoderV2;
+
+contract MyContract {
+    enum ContractState {Active, Suspended}
+    enum State {Available, Rented, Damaged, UnderRepair, NotReturned, Locked, Inspection}
+    State[] statearray = [State.Available, State.Rented, State.Damaged, State.UnderRepair, State.NotReturned, State.Locked, State.Inspection];
+    string[] statename = ["Available", "Rented", "Damaged", "UnderRepair", "NotReturned", "Locked", "Inspection"];
+    
+    address payable wallet;
+    ContractState public currentcontractstate;
+    uint  totalcost;
+    uint  carinrentalprocess;
+    uint currentamountpaid;
+    uint mulresult;
+
+    
+    struct Car {
+        uint _id;
+        string _model;
+        uint _rentalprice;
+        uint _depositfee;
+        State _carstate;
+        address _currentOwner;
+    }
+
+    Car[] carlist;
+
+    constructor(address payable _wallet) public {
+        wallet = _wallet;
+        currentcontractstate = ContractState.Active;
+        carlist.push(Car(0, "Tesla", 7, 14, State.Available, _wallet));
+        carlist.push(Car(1, "Benz", 15, 30, State.Available, _wallet));
+        carlist.push(Car(2, "BMW", 10, 20, State.UnderRepair, _wallet));
+    }
+
+
+    modifier rentcheck(uint _id) {
+        require(carlist[_id]._carstate == State.Available, "This car is currently not availble for rental");
+        _;
+    }
+    
+    modifier returncheck(uint _id) {
+        require(carlist[_id]._carstate == State.Rented, "This car is not rented out");
+        _;
+    }
+    modifier isCompany (address user) {
+        require(wallet == user, "This function can only be called by ABC Rental Company");
+        _;
+    }
+
+    modifier isactive (){
+        require(currentcontractstate == ContractState.Active, "This contract is currently suspended");
+        _;
+    }
+    modifier confirmpaymentcheck(uint accumulatedamount, uint cost) {
+        console.log("The remaining amount yet to pay:", cost-accumulatedamount/1000000000000000000);
+        require(accumulatedamount/1000000000000000000 >= cost, "The amount you have transfered is not enough");
+        _;
+    }
+
+    modifier Conditioncheck (uint _id){
+        require(carlist[_id]._carstate == State.Inspection || carlist[_id]._carstate == State.NotReturned, "This car is neither in inspection status nor not returned status");
+        _;
+    }
+
+    modifier isPositive (uint number){
+        require(number > 0, "Cannot rent for 0 day");
+        _;
+    }
+
+    function listCar() public view returns(Car[] memory){
+
+        for(uint i=0; i<carlist.length; i++){
+            console.log("Car id:", carlist[i]._id);
+            console.log("Car model:", carlist[i]._model);
+            console.log("Rental price per day:", carlist[i]._rentalprice);
+            console.log("Deposit fee:", carlist[i]._depositfee);
+            console.log("Current Status:", statename[uint(carlist[i]._carstate)]);
+        }
+        
+        return carlist;
+    }
+
+    function addCar(string memory _model, uint _rentalprice, uint _depositfee, uint _state) 
+    public isCompany(msg.sender){
+        carlist.push(Car(carlist.length, _model, _rentalprice, _depositfee, statearray[_state], wallet));
+    }
+
+    function removeCar(uint _id)
+    public isCompany(msg.sender){
+        carlist[_id] = carlist[carlist.length-1];
+        carlist[_id]._id = _id;
+        carlist.pop();
+    }
+
+    function showCarStatus(uint _id)
+    view public returns(string memory){
+        return statename[uint(carlist[_id]._carstate)];
+    }
+
+    function showTotalCost(uint _id, uint rentalDays)
+    view public isPositive(rentalDays) returns(uint){
+        safeAdd(safeMul(carlist[_id]._rentalprice, rentalDays),carlist[_id]._depositfee);
+        // safeAdd(mulresult,carlist[_id]._depositfee);
+        //return carlist[_id]._rentalprice*rentalDays+carlist[_id]._depositfee;
+    }
+
+    function rentcar(uint _id, uint rentalDays) 
+    public isactive() rentcheck(_id) isPositive(rentalDays){
+        totalcost = showTotalCost(_id, rentalDays);
+        carinrentalprocess = _id;
+        carlist[_id]._carstate = State.Locked;
+        currentamountpaid = 0;
+        console.log("The total cost is", totalcost, ", please proceed to payment.");
+    }
+
+    function makepayment() public payable{
+        currentamountpaid = safeAdd(currentamountpaid,msg.value);
+        //currentamountpaid += msg.value;
+    }
+
+    function confirmPayment() 
+    public confirmpaymentcheck(currentamountpaid, totalcost) {
+        carlist[carinrentalprocess]._carstate = State.Rented;
+        carlist[carinrentalprocess]._currentOwner = msg.sender;
+        console.log("Finish Payment, Thank you.");
+        wallet.transfer(currentamountpaid-carlist[carinrentalprocess]._depositfee*1000000000000000000);
+        carinrentalprocess = 0;
+        totalcost = 0;
+    }
+
+    function returncar(uint _id) 
+    public returncheck(_id) 
+    returns(uint){
+        carlist[_id]._carstate = State.Inspection;
+        carlist[_id]._currentOwner = wallet;
+        console.log("Car", _id, "is returned and pending a mechanic to conduct an inspection of car condition.");
+    }
+
+    function checkCondition(uint _id, bool _damage, uint maintenanceexpense) 
+    public Conditioncheck(_id){
+        if (carlist[_id]._carstate == State.NotReturned){
+            takeDeposit(_id, carlist[_id]._depositfee);
+        } else if(_damage){
+            carlist[_id]._carstate = State.Damaged;
+            takeDeposit(_id, maintenanceexpense);
+        } else{
+            returnDeposit(_id);
+        }
+    }
+
+    function takeDeposit(uint _id, uint nonrefundableamount) view public {
+        //To be done
+        console.log("Take Deposit", _id, nonrefundableamount);
+    }
+
+    function returnDeposit(uint _id) view public {
+        //To be done
+        console.log("Return Deposit", _id);
+    }
+    
+
+    function carNotReturned(uint _id) 
+    public returncheck(_id) isCompany(msg.sender){
+        if (carlist[_id]._carstate != State.Available){
+            carlist[_id]._carstate = State.NotReturned;
+            console.log("Not return", _id,"The car is not returned after the rental period");
+        }
+    }
+
+    function suspend()
+    public isCompany(msg.sender){ 
+    currentcontractstate == ContractState.Suspended;
+    console.log("Suspend", "The contract is suspended");
+    }
+
+    function activate()
+    public isCompany(msg.sender){ 
+    currentcontractstate == ContractState.Active;
+    console.log("Activate","The contract is activated");
+    }
+
+    function safeMul(uint a, uint b)
+    public pure returns(uint){
+        uint result = a * b;
+        return result;
+    }
+
+    function safeAdd(uint a, uint b)
+    public pure returns(uint){
+        uint result = a + b;
+        return result;
+    }
+
+    function safeMinu(uint a, uint b)
+    public pure returns(uint){
+        uint result = a - b;
+        return result;
+    }
+
+
+}
